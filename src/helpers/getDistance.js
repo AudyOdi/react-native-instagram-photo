6a2fc74d465e451aacce5302dd63b32c7c97b475
audyodi <audytanudjaja@gmail.com>
2017-08-13 20:54:36 +0700

    add getDistance helper

---
+++ src/helpers/getDistance.js
@@ -0,0 +1,16 @@
+// @flow
+
+import type {Touch} from '../SelectedPhoto';
+
+export function pow2abs(a: number, b: number) {
+  return Math.pow(Math.abs(a - b), 2);
+}
+
+function getDistance(touches: Array<Touch>) {
+  const a = touches[0];
+  const b = touches[1];
+
+  return Math.sqrt(pow2abs(a.pageX, b.pageX) + pow2abs(a.pageY, b.pageY));
+}
+
+export default getDistance;
